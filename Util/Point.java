ac3a09ec3e7c0919972c1cfc837a835396fd9c7c
wentaohong <hongwentao0705@gmail.com>
2017-02-27 13:40:20 -0500

    Rectangle Overlap

---
+++ Util/Point.java
@@ -0,0 +1,18 @@
+package Util;
+
+/**
+ * Created by whong53 on 2/27/17.
+ */
+public class Point {
+    public int x, y;
+
+    public Point() {
+        x = 0;
+        y = 0;
+    }
+
+    public Point(int a, int b) {
+        x = a;
+        y = b;
+    }
+}

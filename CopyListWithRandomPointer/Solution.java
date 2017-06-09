6225eb6f7a9bff897cde62f893b704334147cf8a
chendianMyself <1533352873@qq.com>
2017-06-09 14:25:05 +0800

    好
    阿打发斯蒂芬

--- CopyListWithRandomPointer/Solution.java
+++ CopyListWithRandomPointer/Solution.java
@@ -8,12 +8,18 @@ import java.util.Map;
  */
 public class Solution {
     /**
+
+这个软件就是好
      * A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
      * Return a deep copy of the list.
      *
      * @param head: The head of linked list with a random pointer.
      * @return: A new head of a deep copy of the list.
+
      */
+
+
+
     public RandomListNode copyRandomList(RandomListNode head) {
         if (head == null) {
             return null;
